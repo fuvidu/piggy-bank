869250abcb1ab9c0c8e2d6c034683b372960b1ff
fuvidu <fuvidu@gmail.com>
2022-04-17 21:25:03 +0700

    Test case for multiple deposits

--- test/PiggyBank.test.ts
+++ test/PiggyBank.test.ts
@@ -24,6 +24,7 @@ describe("PiggyBank", () => {
       await ethers.getContractFactory("MockAggregator")
     ).deploy();
     await aggregatorContract.deployed();
+    await aggregatorContract.setLatestPrice(price);
 
     piggyBankContract = await (
       await ethers.getContractFactory("PiggyBank")
@@ -32,10 +33,6 @@ describe("PiggyBank", () => {
   });
 
   describe("deposit", () => {
-    beforeEach(async () => {
-      await aggregatorContract.setLatestPrice(price);
-    });
-
     it("should not allow to deposit without a release condition", async () => {
       await expect(
         piggyBankContract.deposit(0, 0, 0, {
@@ -95,8 +92,6 @@ describe("PiggyBank", () => {
 
   describe("withdraw", () => {
     it("should not allow to withdraw release condition is not met", async () => {
-      await aggregatorContract.setLatestPrice(price);
-
       await piggyBankContract.deposit(
         duration,
         takeProfitPercent,
@@ -111,7 +106,6 @@ describe("PiggyBank", () => {
     });
 
     it("should allow to withdraw when duration is expired", async () => {
-      await aggregatorContract.setLatestPrice(price);
       await piggyBankContract.deposit(
         duration,
         takeProfitPercent,
@@ -127,7 +121,6 @@ describe("PiggyBank", () => {
     });
 
     it("should allow to withdraw when price is up to take profit", async () => {
-      await aggregatorContract.setLatestPrice(price);
       await piggyBankContract.deposit(
         duration,
         takeProfitPercent,
@@ -146,7 +139,6 @@ describe("PiggyBank", () => {
     });
 
     it("should allow to withdraw when price is down to stop loss", async () => {
-      await aggregatorContract.setLatestPrice(price);
       await piggyBankContract.deposit(
         duration,
         takeProfitPercent,
@@ -162,6 +154,56 @@ describe("PiggyBank", () => {
       await expect(piggyBankContract.withdraw())
         .to.emit(piggyBankContract, "Withdrawn")
         .withArgs(deployer.address, baseAmount);
+    });
+
+    it("should allow to withdraw only matched amount", async () => {
+      await piggyBankContract.deposit(
+        duration,
+        takeProfitPercent,
+        stopLossPercent,
+        {
+          value: baseAmount,
+        }
+      );
+      await ethers.provider.send("evm_increaseTime", [duration + 1]);
+      await piggyBankContract.deposit(
+        duration,
+        takeProfitPercent,
+        stopLossPercent,
+        {
+          value: baseAmount,
+        }
+      );
+      await expect(piggyBankContract.withdraw())
+        .to.emit(piggyBankContract, "Withdrawn")
+        .withArgs(deployer.address, baseAmount);
+    });
+
+    it("should allow to withdraw only matched amount", async () => {
+      await piggyBankContract.deposit(
+        duration,
+        takeProfitPercent,
+        stopLossPercent,
+        {
+          value: baseAmount,
+        }
+      );
+
+      await piggyBankContract.deposit(
+        duration,
+        takeProfitPercent,
+        stopLossPercent,
+        {
+          value: baseAmount,
+        }
+      );
+
+      const latestPrice = price * (1 + takeProfitPercent / 100 / 100);
+      await aggregatorContract.setLatestPrice(latestPrice);
+
+      await expect(piggyBankContract.withdraw())
+        .to.emit(piggyBankContract, "Withdrawn")
+        .withArgs(deployer.address, baseAmount.mul(2));
     });
   });
 });
